b439267d7146bc7d50bba50b1410d5d449baee35
5hseok <110245258+5hseok@users.noreply.github.com>
2025-06-05 22:35:07 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250605/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 250605/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -6,16 +6,29 @@ def extract_duplication_combination(x, y):
     high_num = max(x,y)
     low_num = min(x,y)
 
-    x_list = []
-    y_list = []
+    x_list = set()
+    y_list = set()
 
-    x_list.append(i for i in range(x-2,x+3))
-    y_list.append(i for i in range(y-2,y+3))
+    for i in range(x-2, x+3):
+        if i < 1:
+            x_list.add(i + N)
+        if i > N:
+            x_list.add(i - N)
+        else:
+            x_list.add(i)
+
+    for i in range(y-2,y+3):
+        if i < 1:
+            y_list.add(i + N)
+        if i > N:
+            y_list.add(i - N)
+        else:
+            y_list.add(i)
 
     result = []
-    for x in x_list:
-        if x in y_list:
-            result.append(x)
-    return len(result) if len(result) != 0 else N - 9
+    for num in range(N):
+        if num not in x_list | y_list:
+            result.append(num)
+    return len(result) if len(result) != 0 else N
 
 print(5**3 * 2 - (extract_duplication_combination(a1, a2) * extract_duplication_combination(b1, b2) * extract_duplication_combination(c1, c2)))
