12741f8e93e2cbd86b8a6d3d223f678e9176384c
5hseok <110245258+5hseok@users.noreply.github.com>
2025-06-05 22:45:34 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250605/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 250605/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -2,33 +2,16 @@ N = int(input())
 a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 
-def extract_duplication_combination(x, y):
-    high_num = max(x,y)
-    low_num = min(x,y)
+def get_valid_num(x):
+    return {(x + i - 1) % N + 1 for i in range(-2, 3)}
 
-    x_list = set()
-    y_list = set()
 
-    for i in range(x-2, x+3):
-        if i < 1:
-            x_list.add(i + N)
-        if i > N:
-            x_list.add(i - N)
-        else:
-            x_list.add(i)
+comb1 = set((i, j, k) for i in get_valid_num(a1)
+                       for j in get_valid_num(b1)
+                       for k in get_valid_num(c1))
 
-    for i in range(y-2,y+3):
-        if i < 1:
-            y_list.add(i + N)
-        if i > N:
-            y_list.add(i - N)
-        else:
-            y_list.add(i)
+comb2 = set((i, j, k) for i in get_valid_num(a2)
+                       for j in get_valid_num(b2)
+                       for k in get_valid_num(c2))
 
-    result = []
-    for num in range(N):
-        if num not in x_list | y_list:
-            result.append(num)
-    return len(result) if len(result) != 0 else N
-
-print(5**3 * 2 - (extract_duplication_combination(a1, a2) * extract_duplication_combination(b1, b2) * extract_duplication_combination(c1, c2)))
+print(len(comb1 | comb2))
