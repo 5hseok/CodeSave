2d17719acd2923fa2f3149abf4dbd2b5d587243c
5hseok <110245258+5hseok@users.noreply.github.com>
2025-06-05 20:36:39 +0900

    Title: 밭의 높이를 고르게하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250605/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 250605/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,9 +1,19 @@
+import sys
+
 N, H, T = map(int, input().split())
 arr = list(map(int, input().split()))
 
-diff_from_H_to_Arr = []
+diff_from_H_to_dict = {}
 
-for num in arr:
-    diff_from_H_to_Arr.append(abs(H-num))
+for i in range(N):
+    diff_from_H_to_dict[i] = abs(H - arr[i])
 
-print(sum(sorted(diff_from_H_to_Arr)[:T]))
+global_sum = sys.maxsize
+
+for i in range(N - T + 1):
+    local_sum = 0
+    for j in range(i, i + T):
+        local_sum += diff_from_H_to_dict.get(j, 0)
+    global_sum = min(local_sum, global_sum)
+
+print(global_sum)
