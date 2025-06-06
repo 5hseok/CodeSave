d8d0538e0a3961a73e84467a2830b8a07fa7e56a
5hseok <110245258+5hseok@users.noreply.github.com>
2025-06-06 17:06:56 +0900

    Title: 개발팀의 능력, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 250606/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -14,9 +14,8 @@ for i in range(len(arr)):
                             sum2 = arr[p] + arr[k]
                             sum3 = sum(arr) - sum1 - sum2
                             diff = max(sum1, sum2, sum3) - min(sum1, sum2, sum3)
-                            if diff != 0:
+                            if sum1 != sum2 and sum1 != sum3 and sum2 != sum3:
                                 global_min = min(global_min, diff)
-
 if global_min != sys.maxsize:
     print(global_min)
 else:
