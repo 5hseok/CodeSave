150f047df397e7c6f334d27666c7cf884da1fd30
5hseok <110245258+5hseok@users.noreply.github.com>
2025-06-06 17:04:01 +0900

    Title: 개발팀의 능력, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250606/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,23 @@
+import sys
+
+arr = list(map(int, input().split()))
+global_min = sys.maxsize
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if i!=j:
+            sum1 = arr[i] + arr[j]
+            for k in range(len(arr)):
+                if k != i and k != j:
+                    for p in range(len(arr)):
+                        if p != k and p != i and p != j:
+                            sum2 = arr[p] + arr[k]
+                            sum3 = sum(arr) - sum1 - sum2
+                            diff = max(sum1, sum2, sum3) - min(sum1, sum2, sum3)
+                            if diff != 0:
+                                global_min = min(global_min, diff)
+
+if global_min != sys.maxsize:
+    print(global_min)
+else:
+    print(-1)
