9bd1bbc9123fc4226fb226c4df01c02c8359e772
5hseok <110245258+5hseok@users.noreply.github.com>
2025-06-06 16:56:44 +0900

    Title: 개발자의 능력 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/개발자의 능력 2/ability-of-developer-2.py
+++ 250606/개발자의 능력 2/ability-of-developer-2.py
@@ -5,13 +5,15 @@ ability = list(map(int, input().split()))
 global_min = sys.maxsize
 
 for i in range(len(ability)):
-    for j in range(i+1, len(ability)):
-        sum1 = ability[i] + ability[j]
-        for k in range(j+1, len(ability)):
-            for p in range(k+1, len(ability)):
-                sum2 = ability[k] + ability[p]
-                sum3 = sum(ability) - sum2 - sum1
-
-    global_min = min(global_min, max(sum1, sum2, sum3) - min(sum1, sum2, sum3))
+    for j in range(len(ability)):
+        if j != i:
+            sum1 = ability[i] + ability[j]
+            for k in range(len(ability)):
+                if k != i and k != j:
+                    for p in range(len(ability)):
+                        if p != i and p != j  and p != k:
+                            sum2 = ability[k] + ability[p]
+                            sum3 = sum(ability) - sum2 - sum1
+                            global_min = min(global_min, max(sum1, sum2, sum3) - min(sum1, sum2, sum3))
 
 print(global_min)
